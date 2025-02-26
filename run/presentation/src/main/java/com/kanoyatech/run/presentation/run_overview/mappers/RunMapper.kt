166e15f40f4a43f42b13a59680e673d07d6133b4
Timothé Larivière <timothe.lariviere@outlook.com>
2025-02-26 09:55:16 +0100

    RunListItem

---
+++ run/presentation/src/main/java/com/kanoyatech/run/presentation/run_overview/mappers/RunMapper.kt
@@ -0,0 +1,34 @@
+package com.kanoyatech.run.presentation.run_overview.mappers
+
+import com.kanoyatech.core.domain.Run
+import com.kanoyatech.core.presentation.ui.formatted
+import com.kanoyatech.core.presentation.ui.toFormattedKm
+import com.kanoyatech.core.presentation.ui.toFormattedKmh
+import com.kanoyatech.core.presentation.ui.toFormattedMeters
+import com.kanoyatech.core.presentation.ui.toFormattedPace
+import com.kanoyatech.run.presentation.run_overview.models.RunUi
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
+
+fun Run.toRunUi(): RunUi {
+    val dateTimeInLocalTime = dateTimeUtc
+        .withZoneSameInstant(ZoneId.systemDefault())
+
+    val formattedDateTime = DateTimeFormatter
+        .ofPattern("MMM dd, yyyy - hh:mma")
+        .format(dateTimeInLocalTime)
+
+    val distanceKm = distanceMeters / 1000.0
+
+    return RunUi(
+        id = id!!,
+        duration = duration.formatted(),
+        dateTime = formattedDateTime,
+        distance = distanceKm.toFormattedKm(),
+        avgSpeed = avgSpeedKmh.toFormattedKmh(),
+        maxSpeed = maxSpeedKmh.toFormattedKmh(),
+        pace = duration.toFormattedPace(distanceKm),
+        totalElevation = totalElevationMeters.toFormattedMeters(),
+        mapPictureUrl = mapPictureUrl
+    )
+}
---
+++ run/presentation/src/main/java/com/kanoyatech/run/presentation/run_overview/models/RunDataUi.kt
@@ -0,0 +1,6 @@
+package com.kanoyatech.run.presentation.run_overview.models
+
+data class RunDataUi(
+    val name: String,
+    val value: String
+)
